45914bf1e253259a4f8615116376315503040e4b
keyserfaty <keserfaty@gmail.com>
2018-11-09 11:21:47 -0500

    add user api

--- src/user.js
+++ src/user.js
@@ -1,6 +1,6 @@
 import { axiosConstructor } from './services/axios'
 import { getUrl } from './config'
 
-const url = getUrl('marketplace')
+const url = getUrl('user')
 
 export const [get, put, post, patch, del] = axiosConstructor(url)
